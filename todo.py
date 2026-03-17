50b16208ffdb5cace15e0cab853ca33030a5b9b3
linneamulkey-bit <linnea.mulkey@student.csd509j.net>
2026-03-17 19:01:46 +0000

    todo

--- todo.py
+++ todo.py
@@ -1,19 +1,13 @@
-todos=[]
+todos=["homework"]
 
 options="""
 1. View todos
 2. Add a todo
-3. Remove a todo"""
+3. Remove a todo
+4. Exit"""
 
 def display_options():
     print(options)
-    choice=input("What do you want to do?")
-    if choice=="1":
-        view_todos()
-    if choice=="2":
-        add_todo()
-    if choice=="3":
-        remove_todo()
 
 def view_todos():
     print(todos)
@@ -26,4 +20,14 @@ def remove_todo():
     delete=input("What do you want to remove?")
     todos.remove(delete)
 
-display_options()
+while True:
+    display_options()
+    choice=input("What do you want to do?")
+    if choice=="1":
+        view_todos()
+    if choice=="2":
+        add_todo()
+    if choice=="3":
+        remove_todo()
+    if choice=="4":
+        break
