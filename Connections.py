c93cd706145e5e74bc80b1312dc1c19ad19f108e
linneamulkey-bit <linnea.mulkey@student.csd509j.net>
2026-05-01 17:34:06 +0000

    connections again again

--- Connections.py
+++ Connections.py
@@ -1,13 +1,9 @@
-catagories={"purple":["[a]","[b]","[c]","[d]"],"yellow":["[e]","[f]","[g]","[h]"],"green":["[i]","[j]","[k]","[l]"],"blue":["[m]","[n]","[o]","[p]"]}
+catagories={"purple":["a","b","c","d"],"yellow":["[e]","[f]","[g]","[h]"],"green":["[i]","[j]","[k]","[l]"],"blue":["[m]","[n]","[o]","[p]"]}
 guessed_purple=[]
 guessed_blue=[]
 guessed_yellow=[]
 guessed_green=[]
 
-guess1="placeholder"
-guess2="placeholder"
-guess3="placeholder"
-guess4="placeholder"
 
 def select():
     print("Choose four things in the list that you think go together and list them in the numbers below.")
@@ -28,11 +24,15 @@ def add(list,color):
 
 def check_num(list1,list2,list3,list4):
     if len(list1)>len(list2) and len(list1)>len(list3) and len(list1)>len(list4):
-        print (f"You have guessed {len(list1)} out of four.")
+        print (f"You have guessed {len(list1)} out of 4.")
 
 while True:
     print(f"categories")
-    select()
+    print("Choose four things in the list that you think go together and list them in the numbers below.")
+    guess1=input("1.")
+    guess2=input("2.")
+    guess3=input("3.")
+    guess4=input("4.")
 
     add(guessed_purple,"purple")
     add(guessed_yellow,"yellow")
@@ -44,15 +44,15 @@ while True:
     check_num(guessed_blue,guessed_purple,guessed_yellow,guessed_green)
     check_num(guessed_yellow,guessed_purple,guessed_blue,guessed_green)
 
-    if len(guessed_blue) is 4:
+    if len(guessed_blue)==4:
         print("Congratulations! You won!")
         break
-    if len(guessed_green) is 4:
+    if len(guessed_green)==4:
         print("Congratulations! You won!")
         break
-    if len(guessed_purple) is 4:
+    if len(guessed_purple)==4:
         print("Congratulations! You won!")
         break
-    if len(guessed_yellow) is 4:
+    if len(guessed_yellow)==4:
         print("Congratulations! You won!")
         break
